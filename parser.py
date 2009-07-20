6c1268e8df38cb10ea6ecdaa7565b5f790310ba4
Anders Conbere <aconbere@gmail.com>
2009-07-19 17:03:11 -1000

    coming along

--- parser.py
+++ parser.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 from __future__ import with_statement
-from os import path
+from os import path, walk
 import simplejson as json
+import yaml
+
+def slugify(string):
+    string = re.sub('\s+', '_', string)
+    string = re.sub('[^\w.-]', '', string)
+    return string.strip('_.- ').lower()
 
 def remove_section(fd):
     section = ""
@@ -11,7 +17,7 @@ def remove_section(fd):
         section += l
         l = fd.readline()
 
-    return section
+    return section.strip()
 
 def remove_header(fd):
     return remove_section(fd)
@@ -20,24 +26,43 @@ def remove_title(fd):
     return remove_section(fd)
 
 def parse_header(header):
-    return json.loads(header)
+    return yaml.load(header)
     
-def parse(file_name):
-    with open(file_name, 'r') as f:
+def parse(filename):
+    data = {}
+    with open(filename, 'r') as f:
         data = parse_header(remove_header(f))
-        data.update({"title": remove_title(f).strip()})
-        data.update({"content": f.read().strip()})
+
+        title = data.get("title") or remove_title(f)
+        slug = data.get("slug") or slugify(title) or slugify(filename)
+        published_on = date.strptime(data.get("published_on"), "%Y-%m-%d")
+        last_modified = date.strptime(data.get("last_modified"), "%Y-%m-%d")
+
+        data.update({"title": title,
+                     "slug": slug,
+                     "published_on": published_on,
+                     "last_modified": last_modified,
+                     "content": f.read().strip()})
     return data
 
-def render(project_path, file_name):
+def render(project_path, filename, data):
     from jinja2 import Environment, FileSystemLoader
-    data = parse(file_name)
 
     templates_path = path.abspath(path.join(project_path, '_templates'))
     env = Environment(loader=FileSystemLoader(templates_path))
     _template = data.get("_template") or "main.html"
     template = env.get_template(_template)
     return template.render(**data)
+
+def parse_project(project_path):
+    for (dirpath, dirnames, filenames) in walk(project_path):
+        if not dirpath.startswith("_"):
+            for filename in filenames:
+                if not filename.startswith("_"):
+                    data = parse(filename)
+                    out = render(project_path, filename, data)
+                    new_file_name = 
+    
 
 if __name__ == "__main__":
     from sys import argv
