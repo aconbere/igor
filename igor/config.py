7dfe7e272f51d3ca6feca7ebb80de91499d42339
Anders Conbere <aconbere@gmail.com>
2009-07-22 10:55:12 -1000

    defaults

--- igor/config.py
+++ igor/config.py
@@ -13,11 +13,11 @@ class Config(object):
         config_path = path.join(self.project_path, self.config_path)
 
         if path.exists(config_path):
-            self.config = defaults.update(self.read(config_path))
+            self.config = self.defaults.update(self.read(config_path))
         else:
             with open(config, 'w') as f:
                 f.write(yaml.dump(self.defaults), default_flow_style=False)
-                self.config = defaults
+                self.config = self.defaults
 
     def get(self, key):
         return self.config.get(key)
