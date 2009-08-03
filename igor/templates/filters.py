0b1912a1398aa254ded5b91116fbea9cf2cdfe82
Anders Conbere <aconbere@gmail.com>
2009-08-03 12:12:42 -1000

    adding rfc3339 filter

--- igor/templates/filters.py
+++ igor/templates/filters.py
@@ -1,5 +1,11 @@
+from rfc3339 import rfc3339
 filters = []
 
 def date(value, format='%H:%M / %d-%m-%Y'):
     return value.strftime(format)
 filters.append(date)
+
+def rfc3339(value):
+    return rfc3339(value)
+filters.append(rfc3339)
+    
