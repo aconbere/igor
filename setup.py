5ddf9a9bb6620a9112eae6e8ec08b27f3af3c981
Anders Conbere <aconbere@gmail.com>
2009-09-02 13:35:04 -1000

    modifying setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name="igor",
       author_email = "aconbere@conbere.org",
       license = "bsd",
       packages=["igor_extras", "igor", "igor.vcs", "igor.vcs.git"],
-      package_data={"igor_extras": ["initial_project/*", "config/*.vhost"]},
+      package_data={"igor_extras": ["initial_project/*", "server_config/*.vhost"]},
       scripts=["scripts/igor", "scripts/igor-post-update"]
      )
