8877716d64b990d9e79b95b0e5e07a27b8c2078b
Anders Conbere <aconbere@gmail.com>
2009-09-02 14:00:54 -1000

    fixing up init command

---
+++ igor_extras/initial_project/media/js/base.js
@@ -0,0 +1,1 @@
+# add js here
--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(name="igor",
       author_email = "aconbere@conbere.org",
       license = "bsd",
       packages=["igor_extras", "igor", "igor.vcs", "igor.vcs.git"],
-      package_data={"igor_extras": ["initial_project/*", "server_config/*.vhost"]},
+      package_data={"igor_extras": ["initial_project/*.yaml",
+                                    "initial_project/_posts/*.txt",
+                                    "initial_project/media/css*.css",
+                                    "initial_project/media/js/*.js",
+                                    "initial_project/_templates/*.html",
+                                    "server_config/*.vhost"]},
       scripts=["scripts/igor", "scripts/igor-post-update"]
      )
