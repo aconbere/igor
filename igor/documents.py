1375ffa485c56405c2cf2f73ec47c401e029aca1
Anders Conbere <aconbere@gmail.com>
2009-12-03 17:04:13 -1000

    messed up an import

--- igor/documents.py
+++ igor/documents.py
@@ -5,8 +5,8 @@ from datetime import datetime
 from uuid import uuid4
 
 import yaml
-from git.log import Log
 
+from vcs.git.log import Log
 from markup import markup
 from utils import slugify, relpath
 
